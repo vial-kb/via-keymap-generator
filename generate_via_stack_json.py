cfdb65d34a94f9a26c0ffaf84a58e9c72e00a60c
Ilya Zhuravlev <whatever@xyz.is>
2021-03-23 22:42:38 -0400

    ensure generated json is sorted

--- generate_via_stack_json.py
+++ generate_via_stack_json.py
@@ -10,22 +10,26 @@ json_files = []
 git.Git("./").clone("https://github.com/the-via/keyboards.git")
 
 for subdir, dirs, files in os.walk("./keyboards/src"):
-    for file in files:
-        path = subdir+"/"+file
+    for filename in files:
+        path = os.path.join(subdir, filename)
         if path.endswith(".json"):
             json_files.append(path)
 
 definitions = {"definitions": {}}
 
-for keyboard in json_files:
-    with open(keyboard, "r") as jf:
-        keyboard_data = json.load(jf)
-        jf.close()
-    vendor_id = int(keyboard_data["vendorId"], 16)
-    product_id = int(keyboard_data["productId"], 16)
-    via_id = vendor_id * 65536 + product_id
-    definitions["definitions"][str(via_id)] = keyboard_data
+for keyboard in sorted(json_files):
+    try:
+        with open(keyboard, "r") as jf:
+            keyboard_data = json.load(jf)
+            jf.close()
+        vendor_id = int(keyboard_data["vendorId"], 16)
+        product_id = int(keyboard_data["productId"], 16)
+        via_id = vendor_id * 65536 + product_id
+        definitions["definitions"][str(via_id)] = keyboard_data
+    except Exception as e:
+        print("WARN", keyboard, e)
+        continue
 
 with open("./via_keyboard_stack.json", "w") as vf:
-    vf.write(json.dumps(definitions, indent=2))
+    vf.write(json.dumps(definitions, indent=2, sort_keys=True))
     vf.close()
